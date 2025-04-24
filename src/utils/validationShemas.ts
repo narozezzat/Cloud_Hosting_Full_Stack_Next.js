c19ac85244e574eff2503a8bc22be8e8dd760c97
Naroz Ezzat <narozezzat10@gmail.com>
2025-04-25 02:09:49 +0300

    feat: implement user registration API with validation and password hashing; add bcrypt and zod for security and validation

--- src/utils/validationShemas.ts
+++ src/utils/validationShemas.ts
@@ -10,3 +10,10 @@ export const createArticleSchema = z.object({
     .max(200, { message: "title should be less than 200 characters" }),
   description: z.string().min(10),
 });
+
+// Register Schema
+export const registerSchema = z.object({
+  username: z.string().min(2).max(100), //.optional(),
+  email: z.string().min(3).max(200).email(),
+  password: z.string().min(6),
+});
